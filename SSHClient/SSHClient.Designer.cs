8a14a821efa069b2c01deb8eadfbc3399c6c71d7
straightcoding <straightcoding@gmail.com>
2024-12-21 15:41:07 -0800

    SSHClient example using SSH.NET

---
+++ SSHClient/SSHClient.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace SSHClient
+{
+    partial class SSHClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.labelCommand = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(592, 10);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(98, 29);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(77, 13);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(156, 21);
+            this.textBoxServer.TabIndex = 1;
+            this.textBoxServer.Text = "39.100.160.218";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server IP";
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(77, 51);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(297, 21);
+            this.textBoxCommand.TabIndex = 3;
+            // 
+            // labelCommand
+            // 
+            this.labelCommand.AutoSize = true;
+            this.labelCommand.Location = new System.Drawing.Point(24, 57);
+            this.labelCommand.Name = "labelCommand";
+            this.labelCommand.Size = new System.Drawing.Size(47, 12);
+            this.labelCommand.TabIndex = 4;
+            this.labelCommand.Text = "Command";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(400, 49);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(98, 29);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResponse.Location = new System.Drawing.Point(12, 110);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.ReadOnly = true;
+            this.textBoxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResponse.Size = new System.Drawing.Size(674, 328);
+            this.textBoxResponse.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Response";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Password";
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(291, 13);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(100, 21);
+            this.textBoxUser.TabIndex = 10;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(469, 13);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 21);
+            this.textBoxPassword.TabIndex = 11;
+            // 
+            // SSHClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxResponse);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.labelCommand);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "SSHClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SSH Client";
+            this.Load += new System.EventHandler(this.SSHClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.Label labelCommand;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxResponse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ SSHClient/SSHClient.cs
@@ -0,0 +1,103 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+using System.Net.Security;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSHClient
+{
+    public partial class SSHClient : Form
+    {
+        private SshClient sshClient;
+        private ShellStream sshStream;
+
+        private Boolean connected = false;
+
+        public SSHClient()
+        {
+            InitializeComponent();
+        }
+
+        private void SSHClient_Load(object sender, EventArgs e)
+        {
+            this.textBoxServer.Text = "";
+            this.textBoxUser.Text = "";
+            this.textBoxPassword.Text = "";
+
+            this.textBoxCommand.Text = "ls";
+        }
+
+        private async void connect()
+        {
+            sshClient = new SshClient(this.textBoxServer.Text, this.textBoxUser.Text, this.textBoxPassword.Text);
+            // need this otherwise will timeout after 10 minutes or so
+            sshClient.KeepAliveInterval = TimeSpan.FromMinutes(1);
+            try
+            {
+                sshClient.Connect();
+                this.buttonConnect.Text = "Disconnect";
+                connected = true;
+
+                sshStream = sshClient.CreateShellStream("Tail", 0, 0, 0, 0, 1024);
+
+                var outputTask = ProcessOutputAsync(sshStream);
+                //var inputTask = ProcessInputAsync(sshStream);
+
+                await Task.WhenAll(outputTask);//, inputTask);
+
+                outputTask.Dispose();
+                sshStream.Dispose();
+                sshClient.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}\r\n{ex.StackTrace}");
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (connected && (sshClient != null))
+            {
+                sshStream.Dispose();
+                sshClient.Disconnect();
+
+                this.buttonConnect.Text = "Connect";
+                connected = false;
+                return;
+            }
+
+            this.textBoxResponse.Text = "";
+
+            connect();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (sshStream.CanWrite)
+                sshStream.WriteLine(this.textBoxCommand.Text);
+        }
+
+        private async Task ProcessOutputAsync(ShellStream shellStream)
+        {
+            using (var reader = new StreamReader(shellStream))
+            {
+                while (shellStream.CanRead)
+                {
+                    if (!shellStream.DataAvailable)
+                        await Task.Delay(100);
+                    else
+                    {
+                        long count = shellStream.Length;
+                        for (int i = 0; i < count; i++)
+                        {
+                            int ch = reader.Read();
+                            this.textBoxResponse.Text += (char)(ch);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
